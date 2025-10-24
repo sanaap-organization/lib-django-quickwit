d749975a5b796251a2770c74268994644012469e
shaqayegh-gh <ghorbanpoor.shaghayegh@gmail.com>
2025-10-24 15:06:41 +0330

    feat(version): upgrade package version

--- django_quickwit_log/__init__.py
+++ django_quickwit_log/__init__.py
@@ -4,7 +4,7 @@ Django Quickwit Log Package
 A Django package for integrating Quickwit with JSON log management and MinIO storage.
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Sanaap Organization"
 __email__ = ""
 
